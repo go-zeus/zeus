f3c3a5224f540959350f9b3bc41920832169661c
如水 <rushui@qq.com>
2024-11-28 21:47:46 +0800

    Update app.go

--- configs/app.go
+++ configs/app.go
@@ -1,12 +1,22 @@
 package configs
 
-import (
-	"github.com/go-zeus/zeus/client"
-	"github.com/go-zeus/zeus/server"
-)
-
 type App struct {
 	Name    string
-	servers []server.Server
-	clients []client.Client
+	servers []Server
+	clients []Client
+}
+
+type Server struct {
+	Name string
+	Port string
+	Ip   string
+}
+
+type Client struct {
+	Name     string
+	balancer Balancer
+}
+
+type Balancer struct {
+	Name string
 }
